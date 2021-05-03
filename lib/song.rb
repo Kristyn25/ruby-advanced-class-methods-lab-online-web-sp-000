cd446c3cb9d1dea2880bbce9da9e022ab28a9396
Kristyn Little <kristynlittle@frontier.com>
2021-05-03 02:20:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,8 +29,7 @@ class Song
   end
 
   def self.find_by_name(name)
-    song = self.name
-    song.name = name
-    song
+    result = self.all.detect {|name| song.name == name}
+    result
   end
 end
